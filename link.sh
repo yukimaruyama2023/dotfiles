84d5e9c87e10043493a35c48a7ad0c4d27419c89
yukimaruyama2023 <yukikeio2023@sslab.ics.keio.ac.jp>
2024-04-03 11:57:21 +0900

    dotfileのリンクミスを修正

--- link.sh
+++ link.sh
@@ -14,3 +14,8 @@ fi
 
 ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/.config/nvim/colors/iceberg.vim ~/.config/nvim/colors/iceberg.vim
+
+# Vim-Plug のインストール．Lazy-Vim を使うときには必要なくなるだろう．
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
