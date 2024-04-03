b676884c3deae33f80f0f0cb2d84e1ae8051cadf
yukimaruyama2023 <yukikeio2023@sslab.ics.keio.ac.jp>
2024-04-03 11:12:21 +0900

    dotfileのリンクを増やした

--- link.sh
+++ link.sh
@@ -3,3 +3,14 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf  ~/.tmux.conf
+
+if [ ! -d ~/.config/nvim ]; then
+    mkdir -p ~/.config/nvim
+fi
+
+if [ ! -d ~/.config/nvim/colors ]; then
+    mkdir -p ~/.config/nvim/colors
+fi
+
+ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/.config/nvim/colors/iceberg.vim ~/.config/nvim/colors/iceberg.vim
