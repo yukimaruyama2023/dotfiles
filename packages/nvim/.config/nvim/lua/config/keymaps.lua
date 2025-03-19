27bd06feae6c450079e141e5a91d65d3f29df6ce
yukimaruyama2023 <yukikeio2023@sslab.ics.keio.ac.jp>
2025-03-19 10:48:34 +0900

    modify ls option

--- packages/nvim/.config/nvim/lua/config/keymaps.lua
+++ packages/nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("i", "jj", "<ESC>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<C-l>", function()
+  print(vim.fn.expand("%:p"))
+end, { noremap = true, silent = true })
--- packages/nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ packages/nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,22 @@
 return {
   { "rebelot/kanagawa.nvim" },
-  { "catppuccin/nvim" },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha", -- choose from "mocha", "macchiato", "frappe", "latte"
+    },
+  },
   {
     "folke/tokyonight.nvim",
     lazy = true,
     opts = { style = "night" },
   },
-
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
+      -- colorscheme = "tokyonight",
     },
   },
 }
